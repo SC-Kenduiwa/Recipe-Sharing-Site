4b321025dfeb4b095141b3157371499d2a9276dc
Shanice Kenduiwa <kenduiwashanice64@gmail.com>
2024-08-03 15:44:54 +0300

    refactored the footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Ensure Link is imported from react-router-dom
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
-const Footer = () => (
-    <footer className="footer">
-        <div className="container">
-            <p>RecipeConnect aims to make cooking enjoyable...</p>
-            <div className="footer-links">
-                <div className="top-links">
-                    <Link to="/">Home</Link>
-                    <Link to="/recipes">Recipes</Link>
-                    <Link to="/profile">Profile</Link>
-                </div>
-                <div className="social-links">
-                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+const Footer = () => {
+    const socialLinks = [
+        { href: "https://www.facebook.com", name: "Facebook" },
+        { href: "https://www.instagram.com", name: "Instagram" },
+        { href: "https://www.twitter.com", name: "Twitter" }
+    ];
+
+    const renderSocialLinks = () =>
+        socialLinks.map(({ href, name }) => (
+            <a key={name} href={href} target="_blank" rel="noopener noreferrer">{name}</a>
+        ));
+
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>RecipeConnect aims to make cooking enjoyable...</p>
+                <div className="footer-links">
+                    <div className="top-links">
+                        <Link to="/">Home</Link>
+                        <Link to="/recipes">Recipes</Link>
+                        <Link to="/profile">Profile</Link>
+                    </div>
+                    <div className="social-links">
+                        {renderSocialLinks()}
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
-);
+        </footer>
+    );
+};
 
 export default Footer;
