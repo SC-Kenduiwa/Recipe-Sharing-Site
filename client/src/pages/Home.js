7e47e86098c84c79f8b179c5d6ea240b7dd581eb
Shanice Kenduiwa <kenduiwashanice64@gmail.com>
2024-08-03 14:50:27 +0300

    Added Assets and created the Home Component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+import RecipeCard from '../components/RecipeCard';
+import './Home.css';
+
+const Home = () => (
+    <div className="home">
+        <img src="/assets/images/featured-image.jpg" alt="Featured" className="featured-image" />
+        <SearchBar />
+        <div className="featured-recipes">
+            <h2>Featured Recipes</h2>
+            <div className="recipe-cards">
+                <RecipeCard image="/assets/images/fried-spaghetti.jpg" title="Fried Spaghetti" description="The ultimate hangover recipe" />
+                <RecipeCard image="/assets/images/meat-deluxe-pizza.jpg" title="Meat Deluxe Pizza" description="The ultimate hangover recipe" />
+                <RecipeCard image="/assets/images/grilled-chicken.jpg" title="Grilled Chicken" description="The ultimate hangover recipe" />
+            </div>
+        </div>
+    </div>
+);
+
+export default Home;
