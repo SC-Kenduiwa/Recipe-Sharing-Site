567be13a60af91a74a464a6ffc280aeff5a2801c
Shanice Kenduiwa <kenduiwashanice64@gmail.com>
2024-08-06 23:01:25 +0300

    rewrote footer and deleted components not needed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
 
 const Footer = () => {
-    const socialLinks = [
-        { href: "https://www.facebook.com", name: "Facebook", icon: faFacebook },
-        { href: "https://www.instagram.com", name: "Instagram", icon: faInstagram },
-        { href: "https://www.twitter.com", name: "Twitter", icon: faTwitter }
-    ];
-
-    const renderSocialLinks = () =>
-        socialLinks.map(({ href, name, icon }) => (
-            <a key={name} href={href} target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={icon} size="2x" />
-            </a>
-        ));
-
-    return (
-        <footer className="footer">
-            <div className="container">
-                <p className="footer-text">RecipeConnect aims to make cooking enjoyable...</p>
-                <div className="footer-links">
-                    <div className="top-links">
-                        <h4>Top Links</h4>
-                        <Link to="/">Home</Link>
-                        <Link to="/about">About</Link>
-                        <Link to="/recipes">Recipes</Link>
-                        <Link to="/profile">Profile</Link>
-                    </div>
-                    <div className="social-links">
-                        {renderSocialLinks()}
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p>RecipeConnect</p>
+      <p>Contact Information: info@recipeconnect.com</p>
+      <p>Follow us on social media</p>
+    </footer>
+  );
 };
 
 export default Footer;
