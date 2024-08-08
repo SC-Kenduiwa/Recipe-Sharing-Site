4a25f40a0be4e6acccbb54bb825abc01898724f6
VenusBridget <venusbri27@gmail.com>
2024-08-08 13:35:09 +0300

    adding filter function

--- client/src/pages/Recipes.js
+++ client/src/pages/Recipes.js
@@ -1,34 +1,64 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import recipepage1 from '../assets/images/recipepage1.jpg'
 import RecipeCard from './RecipeCard'
 import SearchRecipe from './SearchRecipe'
+import Filter from './Filter'
 import './Recipes.css'
 
 function Recipes() {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  
+  const [showFilter, setShowFilter] = useState(false);
+  const [filters, setFilters] = useState({
+    country: '',
+    ingredients: '',
+    createdDateTime: ''
+  });
+
   useEffect(() => {
-    fetch ('http://127.0.0.1:5555/recipes')
-     .then(response => response.json())
-     .then(data => setRecipes(data.recipes))
+    fetch('http://127.0.0.1:5555/recipes')
+      .then(response => response.json())
+      .then(data => setRecipes(data.recipes))
   }, []);
 
   const handleSearchChange = (searchInput) => {
-      setSearch(searchInput)
+    setSearch(searchInput)
   };
 
-  const searchedRecipes = recipes.filter(
-      (recipe) => recipe.title.toLowerCase().includes(search.toLowerCase()) ||
-      recipe.ingredients.toLowerCase().includes(search.toLowerCase())
-  );
+  const handleFilterToggle = () => {
+    setShowFilter(prevState => !prevState);
+  };
+
+  const handleFilterApply = (filterValues) => {
+    setFilters(filterValues);
+    setShowFilter(false);
+  };
+
+  const filteredAndSearchedRecipes = recipes.filter(recipe => {
+    const matchesCountry = filters.country === '' || recipe.country.toLowerCase().includes(filters.country.toLowerCase());
+    const matchesIngredients = filters.ingredients === '' || recipe.ingredients.toLowerCase().includes(filters.ingredients.toLowerCase());
+    const matchesDate = filters.createdDateTime === '' || recipe.createdDateTime === filters.createdDateTime;
+
+    const matchesSearch = recipe.title.toLowerCase().includes(search.toLowerCase()) ||
+      recipe.ingredients.toLowerCase().includes(search.toLowerCase());
+
+    return matchesCountry && matchesIngredients && matchesDate && matchesSearch;
+  });
 
   return (
     <div className='recipe-container'>
-      <img src={recipepage1} alt='recipepage' className='recipe-image'/>
+      <img src={recipepage1} alt='recipepage' className='recipe-image' />
       <h2 className='heading'>Recipes</h2>
-      <SearchRecipe search={search} onSearchChange={handleSearchChange}/>
-      <RecipeCard recipes={searchedRecipes}/>
+      <SearchRecipe search={search} onSearchChange={handleSearchChange} />
+      <button className='filter-button' onClick={handleFilterToggle}>
+        {showFilter ? 'Hide Filters' : 'Show Filters'}
+      </button>
+      {showFilter && <Filter onFilter={handleFilterApply}/>}
+      {filteredAndSearchedRecipes.length > 0 ? (
+        <RecipeCard recipes={filteredAndSearchedRecipes} />
+      ) : (
+        <p className='no-results-message'>No recipes found matching your criteria.</p>
+      )}
     </div>
   )
 }
