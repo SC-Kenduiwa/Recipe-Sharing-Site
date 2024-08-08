da99acef96476a79879f58a64aa70f551bf53d4c
VenusBridget <venusbri27@gmail.com>
2024-08-08 13:45:51 +0300

    resolving conflicts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ import './Navbar.css';
 import Logo from './Logo';
 
 const Navbar = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <nav className="navbar">
@@ -17,21 +17,6 @@ const Navbar = () => {
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
-<<<<<<< HEAD
-
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#recipes">Our Recipes</a></li>
-        <li><a href="#profile">Profile</a></li>
-      </ul>
-      <div className="navbar-buttons">
-        <Link to="#login">
-          <button className="login-btn">Login</button>
-        </Link>
-        <Link to="#signup">
-          <button className="signup-btn">Sign Up</button>
-        </Link>
-
-=======
         <li><Link to="/about">About Us</Link></li>
         <li><Link to="/recipes">Our Recipes</Link></li>
         <li><Link to="/profile">Profile</Link></li>
@@ -39,7 +24,6 @@ const Navbar = () => {
       <div className="navbar-buttons">
         <Link to="/login" className="login-btn">Login</Link>
         <Link to="/signup" className="signup-btn">Sign Up</Link>
->>>>>>> Shanice
       </div>
     </nav>
   );
