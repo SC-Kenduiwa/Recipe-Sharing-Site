969d2664f3b34fd4ecfee6506eddaec2aca83ab3
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-08-09 18:13:19 +0300

    Added RecipeCard component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
+import RecipeCard from './RecipeCard';  
 import './Profile.css';
 
 function UserProfile() {
@@ -69,20 +70,7 @@ function UserProfile() {
         <div className="recipes">
           <h2>My Recipes</h2>
           {recipes.length > 0 ? (
-            recipes.map(recipe => (
-              <div key={recipe.id} className="recipe-item">
-                <div className="recipe-image-container">
-                  <img src={recipe.recipe_image_url} alt={recipe.title} className="recipe-image" />
-                </div>
-                <div className="recipe-details">
-                  <h3>{recipe.title}</h3>
-                  <p>{recipe.description}</p>
-                  <p><strong>Cooking Time:</strong> {recipe.cooking_time} minutes</p>
-                  <p><strong>Difficulty:</strong> {recipe.difficulty_level}</p>
-                  <p><strong>Country:</strong> {recipe.country}</p>
-                </div>
-              </div>
-            ))
+            <RecipeCard recipes={recipes} />
           ) : (
             <p>No recipes added yet.</p>
           )}
@@ -94,20 +82,7 @@ function UserProfile() {
         <div className="bookmarks">
           <h2>Bookmarks</h2>
           {bookmarks.length > 0 ? (
-            bookmarks.map(bookmark => (
-              <div key={bookmark.id} className="bookmark-item">
-                <div className="recipe-image-container">
-                  <img src={bookmark.recipe_image_url} alt={bookmark.title} className="recipe-image" />
-                </div>
-                <div className="recipe-details">
-                  <h3>{bookmark.title}</h3>
-                  <p>{bookmark.description}</p>
-                  <p><strong>Cooking Time:</strong> {bookmark.cooking_time} minutes</p>
-                  <p><strong>Difficulty:</strong> {bookmark.difficulty_level}</p>
-                  <p><strong>Country:</strong> {bookmark.country}</p>
-                </div>
-              </div>
-            ))
+            <RecipeCard recipes={bookmarks} />
           ) : (
             <p>No bookmarks added yet.</p>
           )}
